22e1dc44b4079a21640938439a928cb0623d5de3
Ismael Shakverdiev <me@theiskaa.com>
2023-02-21 23:28:55 +0400

    feat(test): add missing test implementation of field_suggestion.dart

--- test/field_suggestion_test.dart
+++ test/field_suggestion_test.dart
@@ -4,19 +4,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:field_suggestion/field_suggestion.dart';
 
 void main() {
-  late Widget mainWidget;
+  const mockNetworkRequestDuration = 1;
+
+  late Widget mainWidget, mainWidgetNetwork;
   late FieldSuggestion fieldSuggestion;
+  late FieldSuggestion fieldSuggestionNetwork;
 
   late BoxController boxController;
   late TextEditingController textEditingController;
+  late TextEditingController textEditingControllerNetwork;
 
   final fFieldSuggestion = find.byKey(Key('suggestion.field'));
+  final fFieldSuggestionNetwork = find.byKey(Key('suggestion.field.network'));
 
   final suggestions = List.generate(5, (i) => 'test$i@gmail.com');
+  Future<List<String>> suggestionsFuture(String input) => Future.delayed(
+        const Duration(seconds: mockNetworkRequestDuration ),
+        () => suggestions.where((e) => e.contains(input)).toList(),
+      );
 
   setUpAll(() {
     boxController = BoxController();
     textEditingController = TextEditingController();
+    textEditingControllerNetwork = TextEditingController();
 
     fieldSuggestion = FieldSuggestion(
       key: Key('suggestion.field'),
@@ -33,9 +43,23 @@ void main() {
       },
     );
 
-    mainWidget = MaterialApp(
-      home: Scaffold(body: Center(child: fieldSuggestion)),
+    fieldSuggestionNetwork = FieldSuggestion<String>.network(
+      key: Key('suggestion.field.network'),
+      initialData: suggestions,
+      textController: textEditingControllerNetwork,
+      future: (input) => suggestionsFuture(input),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: Text('Loading ...'));
+        }
+
+        final result = snapshot.data ?? [];
+        return ListView(children: [for (final element in result) Text(element)]);
+      },
     );
+
+    mainWidget = MaterialApp(home: Scaffold(body: Center(child: fieldSuggestion)));
+    mainWidgetNetwork = MaterialApp(home: Scaffold(body: Center(child: fieldSuggestionNetwork)));
   });
 
   group('[FieldSuggestion]', () {
@@ -54,8 +78,8 @@ void main() {
 
       expect(find.byType(Material), findsNWidgets(7));
       expect(find.byType(Overlay), findsOneWidget);
-      expect(find.byType(SlideTransition), findsNWidgets(2));
-      expect(find.byType(SizedBox), findsNWidgets(6));
+      expect(find.byType(SlideTransition), findsOneWidget);
+      expect(find.byType(SizedBox), findsNWidgets(7));
       expect(find.byType(Container), findsNWidgets(7));
       expect(find.byType(CompositedTransformTarget), findsNWidgets(2));
       expect(find.byType(ConstrainedBox), findsNWidgets(3));
@@ -63,7 +87,6 @@ void main() {
       expect(find.byType(ListView), findsOneWidget);
       expect(find.byType(TextFormField), findsOneWidget);
 
-      expect(find.byType(Card), findsNWidgets(5));
       expect(find.text('item.0'), findsOneWidget);
       expect(find.text('item.1'), findsOneWidget);
       expect(find.text('item.2'), findsOneWidget);
@@ -88,6 +111,53 @@ void main() {
       // Test closing suggestion box, by giving empty input.
       await tester.enterText(fFieldSuggestion, '');
       await tester.pumpAndSettle();
+    });
+
+    group('.network', () {
+      testWidgets('should contain initial values', (WidgetTester tester) async {
+        const duration = Duration(seconds: mockNetworkRequestDuration * 2);
+        await tester.pumpWidget(mainWidgetNetwork);
+
+        // mainWidget tests.
+        expect(find.byType(MaterialApp), findsOneWidget);
+        expect(find.byType(Scaffold), findsOneWidget);
+        expect(find.byType(Center), findsOneWidget);
+        expect(fFieldSuggestionNetwork, findsOneWidget);
+
+        // Enter text to fieldSuggestion and reload page.
+        await tester.enterText(fFieldSuggestionNetwork, 'test');
+        await tester.pumpAndSettle();
+
+        // Expect loading state. i.e [ConnectionState.waiting].
+        expect(find.byType(Center), findsNWidgets(2));
+        expect(find.text('Loading ...'), findsOneWidget);
+
+        expect(find.byType(Material), findsNWidgets(2));
+        expect(find.byType(Overlay), findsOneWidget);
+        expect(find.byType(SlideTransition), findsNothing);
+        expect(find.byType(SizedBox), findsNWidgets(3));
+        expect(find.byType(Container), findsNWidgets(2));
+        expect(find.byType(CompositedTransformTarget), findsNWidgets(2));
+        expect(find.byType(ConstrainedBox), findsNWidgets(3));
+        expect(find.byType(Opacity), findsOneWidget);
+        expect(find.byType(ListView), findsNothing);
+        expect(find.byType(TextFormField), findsOneWidget);
+
+        await tester.pumpAndSettle(duration);
+
+        expect(find.byType(ListView), findsOneWidget);
+        expect(find.text('test0@gmail.com'), findsOneWidget);
+        expect(find.text('test1@gmail.com'), findsOneWidget);
+        expect(find.text('test2@gmail.com'), findsOneWidget);
+        expect(find.text('test3@gmail.com'), findsOneWidget);
+        expect(find.text('test4@gmail.com'), findsOneWidget);
+
+        // test no results
+        await tester.enterText(fFieldSuggestionNetwork, '3.14');
+        await tester.pumpAndSettle(duration);
+
+        expect(find.byType(Center), findsOneWidget);
+      });
     });
   });
 }
